5e2580c1d4dc56e96028241380f8b4c7f564b404
ingequiroga1 <ingequiroga1@gmail.com>
2024-11-11 15:15:19 -0600

    Se agregan mensajes en Auth

---
+++ assets/inventarioPage-CDUF7euE.js
@@ -0,0 +1,1 @@
+import{_ as n,c as o,a as r,o as t}from"./index-CFeY-9rs.js";const a={};function s(c,e){return t(),o("div",null,e[0]||(e[0]=[r("h1",null,"Inventario",-1)]))}const i=n(a,[["render",s]]);export{i as default};
