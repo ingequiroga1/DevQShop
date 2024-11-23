c4e7490cf8db03109449b670ed172d7490d80f40
ingequiroga1 <ingequiroga1@gmail.com>
2024-11-22 19:50:12 -0600

    Se agregan estilos pro

---
+++ assets/browser-Be4aNeMW.js
@@ -0,0 +1,1 @@
+import{E as e}from"./index-Dm29fpmM.js";var o=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")};const r=e(o),s=Object.freeze(Object.defineProperty({__proto__:null,default:r},Symbol.toStringTag,{value:"Module"}));export{s as b};
