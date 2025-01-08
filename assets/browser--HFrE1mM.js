dd660908c1100533ee90f2f338fea451bf6eb3b3
ingequiroga1 <ingequiroga1@gmail.com>
2025-01-08 06:34:08 -0600

    Se retira ruta para pass

---
+++ assets/browser--HFrE1mM.js
@@ -0,0 +1,1 @@
+import{K as e}from"./index-B1a8dUkk.js";var o=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")};const r=e(o),s=Object.freeze(Object.defineProperty({__proto__:null,default:r},Symbol.toStringTag,{value:"Module"}));export{s as b};
