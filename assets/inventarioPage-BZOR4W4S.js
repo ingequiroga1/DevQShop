716dfe66ded6dc8ce80cec9dcc59e900455d5c29
ingequiroga1 <ingequiroga1@gmail.com>
2025-02-28 20:52:45 -0600

    Se agregan cambios en pdf

--- assets/inventarioPage-BZOR4W4S.js
+++
@@ -1,1 +0,0 @@
-import{_ as n,c as o,a as r,o as t}from"./index-DEselH1G.js";const a={};function s(c,e){return t(),o("div",null,e[0]||(e[0]=[r("h1",null,"Inventario",-1)]))}const i=n(a,[["render",s]]);export{i as default};
--- assets/ventasPage-Cmft0Y6r.js
+++
@@ -1,1 +0,0 @@
-import{_ as n,c as t,a,o as s}from"./index-D3V9Dhf7.js";const o={};function r(c,e){return s(),t("div",null,e[0]||(e[0]=[a("h1",null,"Ventas",-1)]))}const _=n(o,[["render",r]]);export{_ as default};
